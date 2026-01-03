7378f81f03e65013a81324282bbb9ecba87c79e2
anisur426 <rahmanmdanisur069@gmail.com>
2026-01-03 13:16:32 +0600

    class a kicu korcilam

--- Beatuty_Parlour&Salon/book-appointment.php
+++ Beatuty_Parlour&Salon/book-appointment.php
@@ -1,200 +1,109 @@
-<?php 
+<?php
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['bpmsuid']==0)) {
-  header('location:logout.php');
-  } else{
 
-if(isset($_POST['submit']))
-  {
+if (strlen($_SESSION['bpmsuid']) == 0) {
+    header('location:logout.php');
+    exit;
+}
 
-    $uid=$_SESSION['bpmsuid'];
-    $adate=$_POST['adate'];
-    $atime=$_POST['atime'];
-    $msg=$_POST['message'];
+
+$serviceid = isset($_GET['sid']) ? $_GET['sid'] : '';
+$servicename = '';
+
+if ($serviceid != '') {
+    $qry = mysqli_query($con, "SELECT ServiceName FROM tblservices WHERE ID='$serviceid'");
+    if ($row = mysqli_fetch_array($qry)) {
+        $servicename = $row['ServiceName'];
+    }
+}
+
+if (isset($_POST['submit'])) {
+
+    $uid = $_SESSION['bpmsuid'];
+    $adate = $_POST['adate'];
+    $atime = $_POST['atime'];
+    $msg   = $_POST['message'];
     $aptnumber = mt_rand(100000000, 999999999);
-  
-    $query=mysqli_query($con,"insert into tblbook(UserID,AptNumber,AptDate,AptTime,Message) value('$uid','$aptnumber','$adate','$atime','$msg')");
+
+
+    $query = mysqli_query(
+        $con,
+        "INSERT INTO tblbook(UserID,AptNumber,AptDate,AptTime,Message,ServiceID)
+         VALUES('$uid','$aptnumber','$adate','$atime','$msg','$serviceid')"
+    );
 
     if ($query) {
-$ret=mysqli_query($con,"select AptNumber from tblbook where tblbook.UserID='$uid' order by ID desc limit 1;");
-$result=mysqli_fetch_array($ret);
-$_SESSION['aptno']=$result['AptNumber'];
- echo "<script>window.location.href='thank-you.php'</script>";  
-  }
-  else
-    {
-      echo '<script>alert("Something Went Wrong. Please try again")</script>';
+        $_SESSION['aptno'] = $aptnumber;
+        echo "<script>window.location.href='thank-you.php'</script>";
+    } else {
+        echo "<script>alert('Something Went Wrong. Please try again');</script>";
     }
-
-  
 }
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
- 
 
+<head>
     <title>Beauty Parlour Management System | Appointment Page</title>
-
-    <!-- Template CSS -->
     <link rel="stylesheet" href="assets/css/style-starter.css">
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-  </head>
-  <body id="home">
-<?php include_once('includes/header.php');?>
+</head>
 
-<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
-<!--bootstrap working-->
-<script src="assets/js/bootstrap.min.js"></script>
-<!-- //bootstrap working-->
-<!-- disable body scroll which navbar is in active -->
-<script>
-$(function () {
-  $('.navbar-toggler').click(function () {
-    $('body').toggleClass('noscroll');
-  })
-});
-</script>
-<!-- disable body scroll which navbar is in active -->
+<body id="home">
+    <?php include_once('includes/header.php'); ?>
 
-<!-- breadcrumbs -->
-<section class="w3l-inner-banner-main">
-    <div class="about-inner contact ">
-        <div class="container">   
-            <div class="main-titles-head text-center">
-            <h3 class="header-name ">
-                
- Book Appointment
-            </h3>
-            <p class="tiltle-para ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic fuga sit illo modi aut aspernatur tempore laboriosam saepe dolores eveniet.</p>
-        </div>
-</div>
-</div>
-<div class="breadcrumbs-sub">
-<div class="container">   
-<ul class="breadcrumbs-custom-path">
-    <li class="right-side propClone"><a href="index.php" class="">Home <span class="fa fa-angle-right" aria-hidden="true"></span></a> <p></li>
-    <li class="active ">
-        Book Appointment</li>
-</ul>
-</div>
-</div>
-    </div>
-</section>
-<!-- breadcrumbs //-->
-<section class="w3l-contact-info-main" id="contact">
-    <div class="contact-sec	">
-        <div class="container">
+    <section class="w3l-contact-info-main" id="contact">
+        <div class="contact-sec">
+            <div class="container">
+                <div class="d-grid contact-view">
+                    <div class="map-content-9 mt-lg-0 mt-4">
+                        <form method="post">
 
-            <div class="d-grid contact-view">
-                <div class="cont-details">
-                    <?php
 
-$ret=mysqli_query($con,"select * from tblpage where PageType='contactus' ");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
+                            <div style="padding-top: 20px;">
+                                <label>Selected Service</label>
+                                <input type="text" class="form-control"
+                                    value="<?php echo $servicename; ?>" readonly>
+                            </div>
 
-?>
-                    <div class="cont-top">
-                        <div class="cont-left text-center">
-                            <span class="fa fa-phone text-primary"></span>
-                        </div>
-                        <div class="cont-right">
-                            <h6>Call Us</h6>
-                            <p class="para"><a href="tel:+44 99 555 42">+<?php  echo $row['MobileNumber'];?></a></p>
-                        </div>
+                            <div style="padding-top: 20px;">
+                                <label>Appointment Date</label>
+                                <input type="date" class="form-control" name="adate" id="adate" required>
+                            </div>
+
+                            <div style="padding-top: 20px;">
+                                <label>Appointment Time</label>
+                                <input type="time" class="form-control" name="atime" required>
+                            </div>
+
+                            <div style="padding-top: 20px;">
+                                <textarea class="form-control" name="message"
+                                    placeholder="Message" required></textarea>
+                            </div>
+
+                            <button type="submit" class="btn btn-contact" name="submit">
+                                Make an Appointment
+                            </button>
+                        </form>
                     </div>
-                    <div class="cont-top margin-up">
-                        <div class="cont-left text-center">
-                            <span class="fa fa-envelope-o text-primary"></span>
-                        </div>
-                        <div class="cont-right">
-                            <h6>Email Us</h6>
-                            <p class="para"><a href="mailto:example@mail.com" class="mail"><?php  echo $row['Email'];?></a></p>
-                        </div>
-                    </div>
-                    <div class="cont-top margin-up">
-                        <div class="cont-left text-center">
-                            <span class="fa fa-map-marker text-primary"></span>
-                        </div>
-                        <div class="cont-right">
-                            <h6>Address</h6>
-                            <p class="para"> <?php  echo $row['PageDescription'];?></p>
-                        </div>
-                    </div>
-                    <div class="cont-top margin-up">
-                        <div class="cont-left text-center">
-                            <span class="fa fa-map-marker text-primary"></span>
-                        </div>
-                        <div class="cont-right">
-                            <h6>Time</h6>
-                            <p class="para"> <?php  echo $row['Timing'];?></p>
-                        </div>
-                    </div>
-               <?php } ?> </div>
-                <div class="map-content-9 mt-lg-0 mt-4">
-                    <form method="post">
-                        <div style="padding-top: 30px;">
-                            <label>Appointment Date</label>
-                            
-                            <input type="date" class="form-control appointment_date" placeholder="Date" name="adate" id='adate' required="true"></div>
-                        <div style="padding-top: 30px;">
-                            <label>Appointment Time</label>
-                            
-                            <input type="time" class="form-control appointment_time" placeholder="Time" name="atime" id='atime' required="true"></div>
-
-                        <div style="padding-top: 30px;">
-                        <textarea class="form-control" id="message" name="message" placeholder="Message" required=""></textarea></div>
-                        <button type="submit" class="btn btn-contact" name="submit">Make an Appointmen</button>
-                    </form>
                 </div>
-    </div>
-   
-    </div></div>
-</section>
-<?php include_once('includes/footer.php');?>
-<!-- move top -->
-<button onclick="topFunction()" id="movetop" title="Go to top">
-	<span class="fa fa-long-arrow-up"></span>
-</button>
-<script>
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function () {
-		scrollFunction()
-	};
+            </div>
+        </div>
+    </section>
 
-	function scrollFunction() {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			document.getElementById("movetop").style.display = "block";
-		} else {
-			document.getElementById("movetop").style.display = "none";
-		}
-	}
+    <?php include_once('includes/footer.php'); ?>
 
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-		document.body.scrollTop = 0;
-		document.documentElement.scrollTop = 0;
-	}
-$(function(){
-    var dtToday = new Date();
-    
-    var month = dtToday.getMonth() + 1;
-    var day = dtToday.getDate();
-    var year = dtToday.getFullYear();
-    if(month < 10)
-        month = '0' + month.toString();
-    if(day < 10)
-        day = '0' + day.toString();
-    
-    var maxDate = year + '-' + month + '-' + day;
-    $('#adate').attr('min', maxDate);
-});</script>
-<!-- /move top -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script>
+        $(function() {
+            var dtToday = new Date();
+            var month = ('0' + (dtToday.getMonth() + 1)).slice(-2);
+            var day = ('0' + dtToday.getDate()).slice(-2);
+            var year = dtToday.getFullYear();
+            $('#adate').attr('min', year + '-' + month + '-' + day);
+        });
+    </script>
 </body>
 
-</html><?php } ?>
+</html>
